1b90c4a60d48d3a25ef51ba7000704e9a7837bbd
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2021-02-08 17:12:49 -0800

    update comments

--- App.java
+++ App.java
@@ -5,24 +5,29 @@ import SingleResponsibility.SorterManager;
 public class App {
     public static void main(String [] args){
         Book book1 = new Book ("Rp", 127);
+
+        // SINGLE RESPONSIBILITY & OPEN CLOSED PRINCNPLE
         SorterManager sorterManager = new SorterManager();
+        // USAGE
         sorterManager.sort(new MergeSort());
 
+
+        // LISKOV subsitution principle
         // using parent type does NOT break
         ElectricCar prius = new ElectricCar();
         FuelCar mazda = new FuelCar();
 
-       // USING parent type does NOT break the program
+        // USING parent type does NOT break the program
         ElectricVehicle tesla = new ElectricCar();
         FuelVehicle tacoma = new FuelCar();
-
+        // USAGE
         prius.speed();
         mazda.addFuel();
 
         tesla.chargeVehicle();
         tacoma.speed();
 
-        book1.save();
+//        book1.save();
 
 
     }
